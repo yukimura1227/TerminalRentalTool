02f6e351429f2425511be246c72c40f379edab30
yukimura1227 <takamura1227@gmail.com>
2016-01-18 15:13:53 +0000

    vagrant上で開発しているためhostからブラウザアクセスした際にbetter_errorsが動作するようにdev用の設定をメンテ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  BetterErrors::Middleware.allow_ip! "192.168.55.1"
 end
