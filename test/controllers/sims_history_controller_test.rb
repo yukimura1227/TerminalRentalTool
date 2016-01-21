c9347410de4ef4550c8b7c29c87b856d6c41de75
yukimura1227 <takamura1227@gmail.com>
2016-01-21 13:00:12 +0000

    rails generate controller [sims_history]

---
+++ test/controllers/sims_history_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SimsHistoryControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/sims_history_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SimsHistoryHelperTest < ActionView::TestCase
+end
