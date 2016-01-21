29d33b98c0e52ffeda11fcbbd6e57e926a1401ff
yukimura1227 <takamura1227@gmail.com>
2016-01-21 14:35:02 +0000

    apply paper-trail to sim

--- app/controllers/sims_history_controller.rb
+++ app/controllers/sims_history_controller.rb
@@ -1,2 +1,11 @@
 class SimsHistoryController < ApplicationController
+  before_action :set_sim_historys, only: [:index]
+  def index
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_sim_historys
+      @sim = Sim.find(params[:sim_id])
+      @sims_historys = @sim.versions
+    end
 end
