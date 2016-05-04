371f429edeb5c7b102046f110ed1c6be619bcd6b
yukimura1227 <takamura1227@gmail.com>
2016-05-04 11:54:55 +0900

    add helper method [navlink]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def navlink(link, link_name)
+    is_active_page = request.path_info.to_s =~ %r(^#{link}.*)
+    content_tag :a, href: link, class: is_active_page ? 'active-page' : '' do
+      link_name
+    end
+  end
 end
